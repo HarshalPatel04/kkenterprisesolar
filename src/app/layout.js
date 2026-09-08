b0c470c1d31042b88408528138f075f78300bb87
Harshal <hp2726975@gmail.com>
2026-09-08 15:18:29 +0530

    Hero image update

--- src/app/layout.js
+++ src/app/layout.js
@@ -101,11 +101,7 @@ export default function RootLayout({ children }) {
         <Footer />
         <Analytics />
         <SpeedInsights />
-<<<<<<< HEAD
-=======
-
->>>>>>> 60ef174f094b5bf1b7411d44e6bf457454942147
-      </body>
+        </body>
     </html>
   );
 }
